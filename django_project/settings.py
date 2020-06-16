07a7abf0e5eecc251f949e1ad90ce6ed838f3efe
jkenney0501 <jkenney0501@gmail.com>
2020-06-16 16:05:14 -0400

    fix proc file

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '^$wzy=cpcg9txhcy@h6n7p^2*kxc@t7ail+iekr_mfwy4@$fm3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
